54972fa8887a9e40f003db3e13e1121cf0835978
Graham Roberts <robertg4@quest-OptiPlex-9020>
2016-02-13 15:33:16 -0800

    functioning role class, very dumb

---
+++ role.java
@@ -0,0 +1,37 @@
+import java.util.Random.*;
+
+public class role{
+	private int rank;
+	private int taken;
+	private int star;
+	private String name;
+	private String quip;
+
+	role(String N,String Q,int R,int S){
+		this.name=N;
+		this.quip=Q;
+		this.rank=R;
+		this.star=S;
+		this.taken=0;
+	}
+
+	public String name(){
+		return this.name;
+	}
+
+	public String quip(){
+		return this.quip;
+	}
+
+	public int rank(){
+		return this.rank;
+	}
+
+	public int isTaken(){
+		return this.taken;
+	}
+
+	public void setPlayer(){
+		this.taken=1;
+	}
+}
