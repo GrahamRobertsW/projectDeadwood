5fa07a785ef2dc5e403ac5094442cf3b358d7950
Graham W Roberts <grahamwroberts@gmail.com>
2016-02-25 17:54:30 -0800

    fixed Scene

---
+++ Group9_Deadwood/Scene.java
@@ -0,0 +1,61 @@
+package Group9_Deadwood;
+import java.util.*;
+
+public class Scene{
+	private String name;
+	private String desc;
+	private int budget;
+	private HashMap<String, Role> Roles;
+	private int index;
+	private List<Players> players;
+//	private boolean use;      
+	Scene(String input){
+		int rank;
+		String[] temp;
+		Role tempRole;
+		String[] values= input.split("~");
+		name = values[1];
+      desc= values[2];
+		this.players = new List<Players>;
+
+		try{
+			index=Integer.parseInt(values[0]);
+			budget=Integer.parseInt(values[3]);
+		}
+		catch(NumberFormatException e){
+			System.out.format("Error parsing String to int is Scene %s",values[1]);
+		}
+		for (int i=4; i<values.length; i++){
+			temp = values[i].split(",");
+			try{
+				rank=Integer.parseInt(temp[2]);
+            tempRole=new Role(temp[0], temp[1], rank, 1);
+            Roles.put(temp[0], tempRole);
+				}
+			catch(NumberFormatException g){
+				System.out.format("Error could not parse rank in Scene%s, Role%s", values[1], temp[0]);
+			}
+		}
+	}
+
+	public int getBudget(){
+		return this.budget;
+	}
+	
+	public void addPlayer(Player player){		
+		players.add(player);
+	}
+	public List<Players> getPlayers(){
+		return this.players()
+	}
+/*
+	public boolean used(){
+		return this.use;
+	}
+
+	public void used(boolean value){
+		this.use = value;
+	}
+<<<<<<< HEAD
+*/
+}	
