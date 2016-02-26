a0f4354b13c9fdc6526ead350ba4638fa0f18a65
Graham W Roberts <grahamwroberts@gmail.com>
2016-02-25 22:35:53 -0800

    began commenting code

--- Group9_Deadwood/Room.java
+++ Group9_Deadwood/Room.java
@@ -2,6 +2,17 @@ package Group9_Deadwood;
 //import Group9_Deadwood.Role;
 import java.util.*;
 public class Room{
+// class Room
+// This class represents the various rooms on the board
+// Attributes: shots, the number of shot counters on the scene
+// maxShotts the number of shot counters each scene resets to at the beginning of the day
+// extraRoles a hashmap of strings to Role class for the extra roles available
+// Scene the scene object currently in the room
+// name this Room's name and its key into any HashMaps
+// rooms a hashmap of rooms adjacent to this one
+// doors an array of doors that this room is connected to
+// generator a random number generator 
+// complete a boolean describing whether or not a scene has been completed that dfay
    private int shots;
 	private int maxShots;
 	private HashMap<String, Role> extraRoles;
@@ -12,8 +23,14 @@ public class Room{
 	private Random generator;
 	private ArrayList<Players>;
 	private boolean complete;
+
 //	private Scene[] Scenes;
 	Room(String N, Role[] Roles, int S){
+// Constructor
+// passes hardcoded values for a rooms and its scene from the hardcoded Tile class extensions
+// passes a name N,
+// an array of preconstructed Roles that turn into the extras HashMap
+// and the maxShots associated
 		this.name=N;
 		this.maxShots=S;
 		this.shots=this.maxShots;
