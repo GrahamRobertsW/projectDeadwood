5957122f34506b17ed27d9ed9539b106dbd703e8
Graham W Roberts <grahamwroberts@gmail.com>
2016-03-08 13:51:35 -0800

    worked on gui/debugging

--- swingexpirements/TileSwing.java
+++ swingexpirements/TileSwing.java
@@ -5,12 +5,13 @@ import java.awt.*;
 public class TileSwing extends JFrame{
    private TileSwing(){
       this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      this.setSize(2060,1560);
+      this.setSize(1920,1280);
       JPanel panel = new JPanel();
       panel.setLayout(new GridLayout(2,2));
      // panel.setResizeable(true);
       JLabel HotelL = new JLabel();
-      ImageIcon HotelI = new ImageIcon("HotelTile.png");
+		temp = new ImageIcon("HotelTile.png");
+       HotelI 
       HotelL.setIcon(HotelI);
       panel.add(HotelL);
       JLabel MainStL = new JLabel();
@@ -28,8 +29,17 @@ public class TileSwing extends JFrame{
       this.getContentPane().add(panel);
    }
 
+	private JLabel createImgLabel(String filename, width, height){
+		JLabel newLabel = new JLabel();
+		ImageIcon initialIcon = new ImageIcon(filename);
+		BufferedImage bi = new BufferedImage(initialIcon.getWidth(null), initialIcon.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+		Graphiccs g = bi.createGraphics();
+		g.drawImage(initialIcon,0,0, width, height, null);
+		ImageIcon newIcon = new ImageIcon(bi);
+
    public static void main(String[] args){
       new TileSwing().setVisible(true);
    }
+
 }
    
