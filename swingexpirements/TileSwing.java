ad48d65a3835f693503b18c9dbf273c0949d65f6
Graham Roberts <robertg4@cannon.physics.wwu.edu>
2016-03-08 15:38:17 -0800

    almost finished the board maybe

--- swingexpirements/TileSwing.java
+++ swingexpirements/TileSwing.java
@@ -1,41 +1,40 @@
 import java.util.*;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.*;
+
 
 public class TileSwing extends JFrame{
-   private TileSwing(){
-      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      this.setSize(1920,1280);
-      JPanel panel = new JPanel();
-      panel.setLayout(new GridLayout(2,2));
+//   private TileSwing(){
+//      int H=1280;
+//      int W=1920;
+//      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//      this.setSize(W,H);
+//      JPanel panel = new JPanel();
+//      panel.setLayout(new GridLayout(2,2));
      // panel.setResizeable(true);
-      JLabel HotelL = new JLabel();
-		temp = new ImageIcon("HotelTile.png");
-       HotelI 
-      HotelL.setIcon(HotelI);
-      panel.add(HotelL);
-      JLabel MainStL = new JLabel();
-      ImageIcon MainStI = new ImageIcon("MainStTile.png");
-      MainStL.setIcon(MainStI);
-      panel.add(MainStL);
-      JLabel TrainL = new JLabel();
-      ImageIcon TrainI = new ImageIcon("TrainTile.png");
-      TrainL.setIcon(TrainI);
-      panel.add(TrainL);
-      JLabel SecretL = new JLabel();
-      ImageIcon SecretI = new ImageIcon("SecretTile.png");
-      SecretL.setIcon(SecretI);
-      panel.add(SecretL);
-      this.getContentPane().add(panel);
-   }
+//      JLabel HotelL = createImgLabel("HotelTile.png",W/2,H/2);
+//      panel.add(HotelL);
+//      JLabel MainStL = createImgLabel("MainStTile.png",W/2,H/2);
+//      panel.add(MainStL);
+//      JLabel TrainL = createImgLabel("TrainTile.png",W/2,H/2);
+//      panel.add(TrainL);
+//      JLabel SecretL = createImgLabel("SecretTile.png",W/2,H/2);
+//      panel.add(SecretL);
+//      this.getContentPane().add(panel);
+//   }
 
-	private JLabel createImgLabel(String filename, width, height){
+	private JLabel createImgLabel(String filename,int width,int height){
 		JLabel newLabel = new JLabel();
 		ImageIcon initialIcon = new ImageIcon(filename);
-		BufferedImage bi = new BufferedImage(initialIcon.getWidth(null), initialIcon.getHeight(null), BufferedImage.TYPE_INT_ARGB);
-		Graphiccs g = bi.createGraphics();
-		g.drawImage(initialIcon,0,0, width, height, null);
+                Image img = initialIcon.getImage();
+		BufferedImage bi = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+		Graphics g = bi.createGraphics();
+		g.drawImage(img,0,0, width, height, null);
 		ImageIcon newIcon = new ImageIcon(bi);
+                newLabel.setIcon(newIcon);
+                return newLabel;
+        }
 
    public static void main(String[] args){
       new TileSwing().setVisible(true);
