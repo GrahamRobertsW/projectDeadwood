467412891b7ab0c91701070d0138ff69e8c360d6
Graham W Roberts <grahamwroberts@gmail.com>
2016-03-08 17:25:48 -0800

    began the menu gui

---
+++ swingexpirements/menu.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+
+public class SwingMenu extends JFrame{
+	this.setDefaultCloseOperation(JFrame.CLOSE_ON_EXIT);
+   JPanel mainpanel = new JPanel; 
+	mainpanel.setLayout(new boxLayout(mainpanel, boxLayout.X_AXIS));
+
