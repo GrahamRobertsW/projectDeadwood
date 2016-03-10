84b3c7b699adea456934ea5ce8803ba1f3f1e970
Graham Roberts <grahamrobertsw@gmail.com>
2016-03-10 12:18:08 -0500

    argh so close

--- swingexpirements/swingdemo/test.java
+++ swingexpirements/swingdemo/test.java
@@ -4,10 +4,10 @@ import java.util.*;
 import javax.swing.*;
 import java.awt.event.*;
 
-public class test extends JFrame impliments MouseListener, MouseMotionListener{
+public class test extends JFrame implements MouseListener, MouseMotionListener{
 	JLayeredPane layers;
 	JLabel pip;
-	JPanel deadboard;
+	JPanel deadBoard;
 
    //public static void main(String[] args){
    //JFrame frame=new JFrame();
@@ -23,12 +23,49 @@ public class test extends JFrame impliments MouseListener, MouseMotionListener{
   // f//rame.setVisible(true);
    //}
 	public test(){
-		Dimension size() = new Dimension(200,200);
-
 		layers=new JLayeredPane();
 		this.getContentPane().add(layers);
 		layers.addMouseListener(this);
 		layers.addMouseMotionListener(this);
 		deadBoard=new JPanel();
+                layers.add(deadBoard, JLayeredPane.DEFAULT_LAYER);
+                JLabel piece = new JLabel( new ImageIcon("bluepip.png"));
+                JPanel panel = (JPanel) deadBoard.getComponent(0);
+
+        }
+        
+        public static void main(String args[]){
+           JFrame frame = new test();
+           frame.setResizable(false);
+           frame.setVisible(true);
+        }
+
+        public void mousePressed(MouseEvent e){
+           pip=null;
+           Component comp = deadBoard.findComponentAt(e.getX()-20, e.getY()-20);
+           if (comp instanceof JLabel){
+              pip = (JLabel)comp;
+           }
+           return;
+        }
+        
+        public void mouseReleased(MouseEvent e){
+           pip=null;
+        }
+ 
+        public void mouseDragged(MouseEvent e){
+           if (pip==null){
+              return;
+           }
+           pip.setLocation(e.getX()-20, e.getY()-20);
+           return;
+        }
+ 
+        public void mouseClicked(MouseEvent e){}
+        public void mouseMoved(MouseEvent e){}
+        public void mouseEntered(MouseEvent e){}
+        public void mouseExited(MouseEvent e){}
 
 }
+
+
